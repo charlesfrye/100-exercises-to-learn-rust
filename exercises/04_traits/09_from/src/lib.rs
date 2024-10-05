4c184ad1300579718a41ade1fef69291b4f8bd2b
Charles Frye <charles@modal.com>
2024-10-05 16:03:33 -0400

    solution for 04 - 09

--- exercises/04_traits/09_from/src/lib.rs
+++ exercises/04_traits/09_from/src/lib.rs
@@ -1,8 +1,32 @@
-// TODO: Implement the `From` trait for the `WrappingU32` type to make `example` compile.
-
 pub struct WrappingU32 {
     value: u32,
 }
+
+impl From<WrappingU32> for u32 {
+    fn from(w: WrappingU32) -> u32 {
+        w.value
+    }
+}
+
+impl From<u32> for WrappingU32 {
+    fn from(u: u32) -> WrappingU32 {
+        WrappingU32 { value: u }
+    }
+}
+
+// interesting asymmetry -- no reverse blanket impl
+
+// impl Into<WrappingU32> for u32 {
+//     fn into(self) -> WrappingU32 {
+//         WrappingU32 { value: self }
+//     }
+// }
+
+// impl Into<u32> for WrappingU32 {
+//     fn into(self) -> u32 {
+//         self.value
+//     }
+// }
 
 fn example() {
     let wrapping: WrappingU32 = 42.into();
