90db8f696b90889c2fac661b29cc22c61296b263
Charles Frye <charles@modal.com>
2024-10-05 16:56:49 -0400

    solution for 04 - 14

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -8,3 +8,72 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
+pub struct SaturatingU16 {
+    value: u16,
+}
+
+impl From<u16> for SaturatingU16 {
+    fn from(u: u16) -> SaturatingU16 {
+        SaturatingU16 { value: u }
+    }
+}
+
+impl From<&u16> for SaturatingU16 {
+    fn from(pu: &u16) -> SaturatingU16 {
+        SaturatingU16::from(*pu)
+    }
+}
+
+impl From<u8> for SaturatingU16 {
+    fn from(u: u8) -> SaturatingU16 {
+        SaturatingU16::from(u as u16)
+    }
+}
+
+impl From<&u8> for SaturatingU16 {
+    fn from(pu: &u8) -> SaturatingU16 {
+        SaturatingU16::from(*pu as u16)
+    }
+}
+
+impl std::ops::Add for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: SaturatingU16) -> SaturatingU16 {
+        SaturatingU16 {
+            value: self.value.saturating_add(rhs.value),
+        }
+    }
+}
+
+impl std::ops::Add<u16> for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: u16) -> SaturatingU16 {
+        self + SaturatingU16::from(rhs)
+    }
+}
+
+impl std::ops::Add<&u16> for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: &u16) -> SaturatingU16 {
+        self + SaturatingU16::from(rhs)
+    }
+}
+
+impl std::ops::Add<&SaturatingU16> for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: &SaturatingU16) -> SaturatingU16 {
+        self + rhs.value // hmm
+    }
+}
+
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, other: &u16) -> bool {
+        self.value == *other // hmm
+    }
+}
